6ed33b0cb93b2c0d6850c7fd1332d1a32f45ff6e
H Shaktawat <106646547+h-shaktawat-dw@users.noreply.github.com>
2022-11-14 16:01:58 +0530

    Merge PDF Functionality Added
    Merge PDF Functionality Added

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,7 +36,8 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-
+$routes->get('/pdf-merge', 'PdfMergeController::mergePDF');
+$routes->get('/convert-to-pdf', 'PdfMergeController::convertToPdf');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
